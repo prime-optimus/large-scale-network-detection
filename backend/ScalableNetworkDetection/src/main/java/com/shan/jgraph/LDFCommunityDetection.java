6e4742fae06a31f4404318c1ce79e2e5dd9bed77
sgodbo <sgodbole@sapient.com>
2015-10-21 11:32:50 -0400

    maven errors removed

--- backend/ScalableNetworkDetection/src/main/java/com/shan/jgraph/LDFCommunityDetection.java
+++ backend/ScalableNetworkDetection/src/main/java/com/shan/jgraph/LDFCommunityDetection.java
@@ -5,22 +5,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
-import java.util.regex.Pattern;
-
-import sun.text.normalizer.UBiDiProps;
 import jxl.read.biff.BiffException;
 
 public class LDFCommunityDetection {
